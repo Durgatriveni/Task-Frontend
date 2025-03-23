a0238f76b654ce42509da3cc2c32c18c7a961a0d
Durgatriveni <trivenireddy81@gmail.com>
2025-03-23 08:16:51 +0530

    Updated App.jsx with necessary changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
-import Unauthorized from "./pages/Unauthorized";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+// import ProtectedRoute from "./components/ProtectedRoute";
 
+import Home from './pages/Home';
+import Contact from './pages/Contactus';
 function App() {
   return (
-    <Router>
+    <div className="min-h-screen bg-gray-100">
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/unauthorized" element={<Unauthorized />} />
-
-        {/* Protected Routes */}
-        <Route element={<PrivateRoute allowedRoles={["admin", "user"]} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-
-        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-          <Route path="/admin" element={<AdminPanel />} />
-        </Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      
+        <Route path="/home" element={<Home />} />
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
-    </Router>
+    </div>
   );
 }
 
